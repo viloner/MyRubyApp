f8d155cfdf9163cd762c9fa93b7b357b6ebccd62
viloner <viloner@gmail.com>
2011-09-17 14:27:36 +0300

    Same commit

--- app/models/user.rb
+++ app/models/user.rb
@@ -33,13 +33,10 @@ class User < ActiveRecord::Base
         encrypted_password == encrypt(submitted_password)
     end
     
-  class << self
-    def authenticate(email, submitted_password)
-       user = find_by_email(email)
-       return nil  if user.nil?
-       return user if user.has_password?(submitted_password)
+    def self.authenticate(email, submitted_password)
+        user = find_by_email(email)
+        user && user.has_password?(submitted_password) ? user : nil
     end
-  end
 
   private
 
